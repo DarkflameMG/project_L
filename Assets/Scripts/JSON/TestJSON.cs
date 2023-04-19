1836e016570181f7364008f030fab2f77beb276d
DarkflameMG <deknan70@gmail.com>
2023-04-19 20:52:07 +0700

    json test

---
+++ Assets/Scripts/JSON/TestJSON.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TestJSON : MonoBehaviour
+{
+    private void Start() {
+        MissionData t1 = new MissionData();
+        t1.position = new Vector3 (0,0,0);
+        t1.health = 200;
+
+        string json = JsonUtility.ToJson(t1);
+        Debug.Log(json);
+
+        File.WriteAllText(Application.dataPath+"/Data/saveFile.json",json);
+    }
+
+    private class MissionData{
+        public Vector3 position;
+        public int health;
+    }
+}
