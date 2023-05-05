e0fd9144fd2770138570857f51fb5aee99a99582
DarkflameMG <deknan70@gmail.com>
2023-05-05 20:27:08 +0700

    gate drag drop+line connected

--- Assets/Scripts/TestLine/Drag.cs
+++ Assets/Scripts/TestLine/Drag.cs
@@ -5,55 +5,72 @@ using UnityEngine.EventSystems;
 
 public class Drag : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IEndDragHandler,IDragHandler,IPointerUpHandler
 {
-    // [SerializeField]private Canvas canvas;
+    private Canvas canvas;
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
     [SerializeField]private bool isPointer1;
-    private bool isStart = false;
+    private bool isValid = false;
+    private Transform currentSlot;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        // Debug.Log(transform.parent.GetComponent<RectTransform>());
-        isStart = true;
+        Debug.Log(slotPosition());
     }
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("up");
+        // Debug.Log("up");
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Debug.Log("begin");
         canvasGroup.blocksRaycasts = false;
+        isValid = false;
+        currentSlot = null;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         // Debug.Log("end");
         canvasGroup.blocksRaycasts = true;
+        if(!isValid)
+        {
+            Destroy(transform.parent.gameObject);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta ;/// canvas.scaleFactor;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
         // Debug.Log( rectTransform.anchoredPosition);
     }
 
+    public void setValid(Transform slot){
+        isValid = true;
+        currentSlot = slot;
+    }
+
     private void Update() {
-        if(!isPointer1 && !isStart)
+        if(isValid && currentSlot == null)
         {
-            // transform.position = currentMousePosition();
+            Destroy(transform.parent.gameObject);
+        }
+
+        else if(isValid)
+        {
+            GetComponent<RectTransform>().anchoredPosition = slotPosition();
         }
     }
 
-    private Vector3 currentMousePosition()
+    private Vector2 slotPosition()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0;
-        return mousePosition;
+        Vector2 parentSlotPosition = currentSlot.parent.GetComponent<RectTransform>().anchoredPosition;
+        Vector2 currentSlotPostion = currentSlot.GetComponent<RectTransform>().anchoredPosition;
+        return parentSlotPosition+currentSlotPostion;
     }
 }
--- Assets/Scripts/TestLine/Slot.cs
+++ Assets/Scripts/TestLine/Slot.cs
@@ -7,29 +7,41 @@ public class Slot : MonoBehaviour, IDropHandler,IPointerDownHandler,IBeginDragHa
 {
     [SerializeField]private Transform linePre;
     private Transform lineParent;
+    private Transform currentLine;
     private void Awake() {
-        lineParent = GameObject.Find("Line").transform;
+        lineParent = GameObject.Find("spawnLine").transform;
     }
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("drop");
-        eventData.pointerDrag.transform.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        Debug.Log(GetComponent<RectTransform>().anchoredPosition);
+        if(eventData.pointerDrag != null)
+        {
+            Vector2 parentPosition = transform.parent.GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.transform.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition+parentPosition;
+            eventData.pointerDrag.transform.GetComponent<Drag>().setValid(this.transform);
+            currentLine = eventData.pointerDrag.transform.parent;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("down");
-        // Transform newLine = Instantiate(linePre,Vector3.zero,Quaternion.identity,lineParent);
-        // newLine.GetChild(1).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        // newLine.GetChild(2).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        Debug.Log(currentLine);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Transform newLine = Instantiate(linePre,Vector3.zero,Quaternion.identity,lineParent);
-        newLine.GetChild(1).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        newLine.GetChild(2).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        eventData.pointerDrag = newLine.GetChild(2).gameObject;
+        if(currentLine == null)
+        {
+            Vector2 parentPosition = transform.parent.GetComponent<RectTransform>().anchoredPosition;
+            // Debug.Log(parentPosition);
+            Transform newLine = Instantiate(linePre,Vector3.zero,Quaternion.identity,lineParent);
+            newLine.GetChild(1).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition+parentPosition;
+            newLine.GetChild(2).GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition+parentPosition;
+            eventData.pointerDrag = newLine.GetChild(2).gameObject;
+            currentLine = newLine;
+            newLine.GetChild(1).GetComponent<Drag>().setValid(this.transform);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
