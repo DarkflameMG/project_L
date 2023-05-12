de4e8a0f7cd53427c3aefc0c3b24c83aeab6af1b
DarkflameMG <deknan70@gmail.com>
2023-05-12 20:02:22 +0700

    save btn

---
+++ Assets/Scripts/TestLine/SaveMap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveMap : MonoBehaviour
+{
+    private GameObject[] gates;
+    private Button btn;
+    private void Start() {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(saveGates);
+    }
+    private void saveGates()
+    {
+        gates = GameObject.FindGameObjectsWithTag("saveable");
+        Debug.Log(gates.Length);
+    }
+}
--- Assets/Scripts/TestLine/SwitchLight.cs
+++ Assets/Scripts/TestLine/SwitchLight.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Image img;
+    [SerializeField]private Sprite off;
+    [SerializeField]private Sprite on;
+    private GateObject gateObject;
+    private void Awake() {
+        img = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        gateObject = transform.parent.GetComponent<GateObject>();
+        if(gateObject.getCurrentState())
+        {
+            img.sprite = on;
+        }
+        else
+        {
+            img.sprite = off;
+        }
     }
 }
