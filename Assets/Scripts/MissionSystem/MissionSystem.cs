45445e9c2c2020ae3f92e7474bfff6258391c534
DarkflameMG <deknan70@gmail.com>
2023-05-28 20:41:05 +0700

    upgrade preview

--- Assets/Scripts/MissionSystem/MissionSystem.cs
+++ Assets/Scripts/MissionSystem/MissionSystem.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MissionSystem : MonoBehaviour
 {
     [SerializeField]private MissionSO missionSO;
+    [SerializeField]private LobbyInfo lobby;
     public void setCurerntMission(MissionInfo data)
     {
         missionSO.missionInfo = data;
@@ -13,5 +16,18 @@ public class MissionSystem : MonoBehaviour
     public MissionInfo GetMissionInfo()
     {
         return missionSO.missionInfo;
+    }
+
+    private void Update() {
+        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            completeMission();
+        }
+    }
+
+    private void completeMission()
+    {
+        lobby.PlayerLocation = new Vector3(0,0,-7.84f);
+        SceneManager.LoadScene("Lobby");
     }
 }
