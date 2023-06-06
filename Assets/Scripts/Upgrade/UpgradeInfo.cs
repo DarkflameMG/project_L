e9a86f30445b1fee87867f5281bcd394e223b734
DarkflameMG <deknan70@gmail.com>
2023-06-06 20:47:12 +0700

    add mod page

--- Assets/Scripts/Upgrade/UpgradeInfo.cs
+++ Assets/Scripts/Upgrade/UpgradeInfo.cs
@@ -7,10 +7,14 @@ public class UpgradeInfo : MonoBehaviour
 {
     [SerializeField]private LobbyInfo lobbyinfo;
     [SerializeField]private GameObject UI;
+    private GameObject mainUp;
+    private GameObject mod;
     void Awake()
     {
         TextAsset json = Resources.Load<TextAsset>("Data/Upgrade/upgradeSystem");
         Debug.Log(json);
+        mainUp =  UI.transform.GetChild(0).gameObject;
+        mod =  UI.transform.GetChild(1).gameObject;
     }
 
     private void Update() {
@@ -20,15 +24,37 @@ public class UpgradeInfo : MonoBehaviour
             {
                 lobbyinfo.Busy = false;
                 lobbyinfo.currentPage = Page.none;
-                Debug.Log("xxx");
                 UI.SetActive(false);
+            }
+        }
+        else if(lobbyinfo.currentPage == Page.mod)
+        {
+            mainUp.SetActive(false);
+            mod.SetActive(true);
+            if(Keyboard.current.escapeKey.wasPressedThisFrame)
+            {
+                lobbyinfo.Busy = false;
+                lobbyinfo.currentPage = Page.none;
+                UI.SetActive(false);
+            }
+            else if(Keyboard.current.tabKey.wasPressedThisFrame)
+            {
+                lobbyinfo.currentPage = Page.mainUp;
+                resetUI();
             }
         }
     }
 
     public void run()
     {
+        resetUI();
         lobbyinfo.currentPage = Page.mainUp;
         UI.SetActive(true);
+    }
+
+    private void resetUI()
+    {
+        mainUp.SetActive(true);
+        mod.SetActive(false);
     }
 }
