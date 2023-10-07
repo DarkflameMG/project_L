f35ef41cac068be3a030c9e8fd5f00b4410a9fdd
DarkflameMG <deknan70@gmail.com>
2023-10-07 21:48:55 +0700

    add portal to puzzlle creation

---
+++ Assets/Scripts/Interaction/GatePuzzle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GatePuzzle : MonoBehaviour,IInteractable
+{
+    [SerializeField]private string _prompt;
+    [SerializeField]private GameObject popupSystem;
+    public string interactionPrompt => _prompt;
+    public bool Interact(Interactor interactor)
+    {
+        ChangeScene();
+        return true;
+    }
+
+    public bool ShowPopup()
+    {
+        popupSystem.GetComponent<Popups>().showPopup("Enter");
+        return true;
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("TestJSON");
+    }
+}
